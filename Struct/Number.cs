8169ed5120eae68049200c7fb41b10f9ccb633fb
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-06-09 15:27:15 -0300

    Add project files.

---
+++ Struct/Number.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Struct
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Struct/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.Amount = 819.375m;
+
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
